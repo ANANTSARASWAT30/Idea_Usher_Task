25a21f2a52401e2d047df53933dd66d0fd4947e0
ANANTSARASWAT30 <anantsaraswat308@gmail.com>
2024-12-21 22:37:42 +0530

    final changes

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import postRoutes from './routes/postRoutes.js';
 import tagRoutes from './routes/tagRoutes.js';
+import path from 'path';
 
 dotenv.config();
 
@@ -11,8 +12,17 @@ connectDB();
 
 app.use(express.json()); // Body parsing middleware
 
+// Serve Static Files
+app.use(express.static(path.join(path.resolve(), 'public')));
+
+// API Routes
 app.use('/api/posts', postRoutes);
 app.use('/api/tags', tagRoutes);
+
+// Landing Page Route
+app.get('/', (req, res) => {
+    res.sendFile(path.join(path.resolve(), 'public', 'index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
